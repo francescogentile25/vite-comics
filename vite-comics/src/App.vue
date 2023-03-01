3605dae033890bbcb41cef96c5ea46ac480c9c42
francescogentile25 <francescogentile250893@gmail.com>
2023-03-01 15:02:41 +0100

    FEAT add scaffolding

---
+++ vite-comics/src/App.vue
@@ -0,0 +1,31 @@
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+
+}
+</script>
+
+<template>
+  <HeaderComponent></HeaderComponent>
+  <MainComponent></MainComponent>
+  <FooterComponent></FooterComponent>
+</template>
+
+<style scoped></style>
