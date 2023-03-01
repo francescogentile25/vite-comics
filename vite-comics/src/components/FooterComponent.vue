c4f98d1afc24110d85cc1b7aeb07ce45f8b4ff87
francescogentile25 <francescogentile250893@gmail.com>
2023-03-01 17:43:36 +0100

    FEAT add section blue

--- vite-comics/src/components/FooterComponent.vue
+++ vite-comics/src/components/FooterComponent.vue
@@ -1,4 +1,27 @@
-<template></template>
+<template>
+    <ul class="prova container">
+        <li class="nav-item">
+            <img src="/img/buy-comics-digital-comics.png" class="icon" alt="">
+            <p class="icon-txt">Digital comics</p>
+        </li>
+        <li class="nav-item">
+            <img src="/img/buy-comics-merchandise.png" class="icon" alt="">
+            <p class="icon-txt">Digital comics</p>
+        </li>
+        <li class="nav-item">
+            <img src="/img/buy-comics-subscriptions.png" class="icon" alt="">
+            <p class="icon-txt">Digital comics</p>
+        </li>
+        <li class="nav-item">
+            <img src="/img/buy-comics-shop-locator.png" class="icon" alt="">
+            <p class="icon-txt">Digital comics</p>
+        </li>
+        <li class="nav-item">
+            <img src="/img/buy-comics-digital-comics.png" class="icon" alt="">
+            <p class="icon-txt">Digital comics</p>
+        </li>
+    </ul>
+</template>
 
 <script>
 export default {
@@ -10,4 +33,29 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../styles/partials/generic' as*;
+
+.nav-item {
+    align-items: center;
+    display: flex;
+}
+
+.icon {
+    width: 42px;
+    margin-right: 16px;
+}
+
+.icon-txt {
+    color: white;
+    text-transform: uppercase;
+}
+
+.prova {
+    padding: 48px;
+    justify-content: space-between;
+    display: flex;
+    gap: 70px;
+    background-color: blue;
+}
+</style>
