6c3221e404ebdea0e3c1ee7e00e0198d97efac2a
francescogentile25 <francescogentile250893@gmail.com>
2023-03-01 16:02:38 +0100

    STYLE  pushhhhhhh

--- vite-comics/src/components/HeaderComponent.vue
+++ vite-comics/src/components/HeaderComponent.vue
@@ -1,6 +1,15 @@
-<template></template>
+@use './styles/partials/generic' as*;
 
-<script>
+<template>
+    <div class="bg-red">
+        <img src="/img/dc-logo.png" alt="">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita natus eum est nihil! Accusamus incidunt
+            corrupti molestiae laborum ut quod, beatae numquam esse commodi? Aut corporis at consequuntur voluptate!
+        </p>
+    </div>
+</template>
+
+<!-- <script>
 export default {
     data() {
         return {
@@ -8,6 +17,14 @@ export default {
         }
     },
 }
-</script>
+</script> -->
 
-<style></style>
+<style lang="scss" scoped>
+.bg-red {
+    background-color: red;
+}
+
+img {
+    width: 100px;
+}
+</style>
