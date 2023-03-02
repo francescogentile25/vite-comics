7b12935bc550bccb365b9ff9af5cf3781a78646c
francescogentile25 <francescogentile250893@gmail.com>
2023-03-02 16:55:07 +0100

    ERROR

---
+++ src/components/MainCardComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card">
+        <img :src="card.thumb" alt="" class="card-image">
+        <p class="card-title">{{ card.series }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        },
+    },
+    data() {
+        return {
+            saluto: 'ciao',
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/partials/generic' as*;
+
+p {
+    color: white;
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,30 +1,33 @@
 <template>
     <div class="container">
-        <p class="text">component goes here</p>
-        <img src="/img/jumbotron.jpg" alt="" class="jumbo">
+        <MainCardComponent v-for="(card, i) in cards" :key="i"></MainCardComponent>
     </div>
 </template>
 
 <script>
+import MainCardComponent from './MainCardComponent.vue';
 export default {
+    components: {
+        MainCardComponent
+    },
     data() {
         return {
             saluto: 'ciao',
             cards: [
                 {
-                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    "thumb": "https://picsum.photos/200",
                     "price": "$19.99",
                     "series": "Action Comics",
                     "type": "comic book"
                 },
                 {
-                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    "thumb": "https://picsum.photos/200",
                     "price": "$3.99",
                     "series": "American Vampire 1976",
                     "type": "comic book"
                 },
                 {
-                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    "thumb": "https://picsum.photos/200",
                     "price": "$16.99",
                     "series": "Aquaman",
                     "type": "graphic novel"
